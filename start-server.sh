2226e77203437c864b95876e217ca683d8ebdecb
vinurs <haiyuan.vinurs@gmail.com>
2021-01-03 12:25:04 +0800

    增加了启动脚本

---
+++ start-server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#==============================================================================
+#
+#        Filename:  @(#)start-server.sh
+#
+#     Description:  start hugo server for test
+#
+#         Version:  1.0
+#         Created:  2018/03/02 00:54:11
+#         Changed:  <vinurs 03/02/2018 08:54:32>
+#        Revision:  none
+#
+#          Author:  vinurs
+#           Email:  <vinurs@vinurs-MBP.lan>
+#
+#==============================================================================
+##
+
+
+hugo server -D --navigateToChanged
+
