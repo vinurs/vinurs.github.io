3769bc5c1990423c71c49dd41daff0d8e8c9e6b5
vinurs <haiyuan.vinurs@gmail.com>
2021-02-07 06:23:40 +0800

    忽略日记，日记不导出

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ export ALL_PROXY=socks5://127.0.0.1:7891
 rm -rf content/*
 rm -rf static/ox-hugo
 cd ox-hugo; make md; cd -
-# 删除draft
+
 cd content
+# 删除所有的日记，日记不导出
+rm -rf post/diaries
+# 删除draft
 grep -lrn "draft = true" * | xargs rm -rf
 cd -
 
